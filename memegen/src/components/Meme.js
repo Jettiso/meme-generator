13301d5dc6125d87bb794124265558eba4ed74e8
Jettison <ilmsmjettison@gmail.com>
2022-09-11 15:52:36 +0800

    add meme img onClick

--- memegen/src/components/Meme.js
+++ memegen/src/components/Meme.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = useState('')
+
+    function getMemeImg() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        setMemeImage(memesArray[randomNumber].url)
+
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input" placeholder="Top text"/>
                 <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button"> Get a new image</button>
-            </form>
+                <button className="form--button" onClick={getMemeImg}> Get a new image</button>
+            </div>
+            <img src={memeImage} className='meme--image'/>
         </main>
     );
 }
