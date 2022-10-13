d9d9b2bf3fa744ef319e90b854021580b313b59c
Jettison <ilmsmjettison@gmail.com>
2022-10-13 16:21:31 +0800

    allow input text on img

--- memegen/src/components/Meme.js
+++ memegen/src/components/Meme.js
@@ -9,6 +9,14 @@ export default function Meme() {
     });
     const [allMemeImages, setAllMemeImages] = useState(memesData);
 
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme, 
+            [name] : value
+        }))
+    }
+
     function getMemeImg() {
         const memesArray = allMemeImages.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
@@ -26,11 +34,17 @@ export default function Meme() {
                     type="text"
                     className="form--input"
                     placeholder="Top text"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input
                     type="text"
                     className="form--input"
                     placeholder="Bottom text"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button className="form--button" onClick={getMemeImg}>
                     Get a new image
@@ -38,8 +52,8 @@ export default function Meme() {
             </div>
             <div className="meme">
             <img src={meme.randomImage} className="meme--image" />
-            <h2 className="meme--text top"> One does not simply</h2>
-            <h2 className="meme--text bottom"> Walk into Mordor</h2>
+            <h2 className="meme--text top"> {meme.topText}</h2>
+            <h2 className="meme--text bottom"> {meme.bottomText}</h2>
             </div>
         </main>
     );
